c739fbd94e55c65c4d7c11338ba408d1ef4c29e6
AmitaiYacobi <amitaiyacobi@gmail.com>
2021-11-01 04:00:29 +0200

    fix some problems

--- ex1.py
+++ ex1.py
@@ -1,20 +1,20 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import sys
+import os
 
 image_fname, centroids_fname, out_fname = sys.argv[1], sys.argv[2], sys.argv[3]
-centroids = np.loadtxt(centroids_fname)  # load centroids
+centroids = np.loadtxt(centroids_fname)
 orig_pixels = plt.imread(image_fname)
 pixels = orig_pixels.astype(float) / 255.
-# Reshape the image(128x128x3) into an Nx3 matrix where N = number of pixels.
 pixels = pixels.reshape(-1, 3)
-print(centroids)
 
 
 def Kmeans():
-    print(len(pixels))
     output_file = open(out_fname, "w")
     iteration = 0
+    prev_centroids = centroids.copy()
+
     while iteration < 20:
         counter = 0
         clusters = [[] for centroid in centroids]
@@ -24,34 +24,30 @@ def Kmeans():
             distances = []
             # calculate the distances between the current pixel to each centroid
             for centroid in centroids:
-                distance = np.sqrt(np.sum(np.square(centroid - pixel)))
+                distance = np.linalg.norm(pixel - centroid)
                 distances.append(distance)
             min_distance = min(distances)
-
             # the index of the closest centroid
             index = distances.index(min_distance)
             clusters[index].append(pixel)
 
         # update each centroid according to the avarage of its cluster
         for i in range(len(clusters)):
-            sum_of_pixels = sum(clusters[i])
-            average = sum_of_pixels / len(clusters[i])
+            average = np.average(clusters[i], axis=0)
+            centroids[i] = centroids[i].round(4)
+            average = average.round(4)
+            centroids[i] = average.copy()
 
-            if (centroids[i] == average).all():
-                counter += 1
-            centroids[i] = average
+        output_file.write(
+            f"[iter {iteration}]:{','.join([str(i.round(4)) for i in centroids])}\n"
+        )
 
-        # convegence
-        if counter == len(centroids):
+        # check convegence
+        if np.array_equal(centroids, prev_centroids):
             break
         else:
-            print(
-                f"[iter {iteration}]:{','.join([str(i.round(4)) for i in centroids])}\n"
-            )
-            output_file.write(
-                f"[iter {iteration}]:{','.join([str(i) for i in centroids])}\n"
-            )
-        iteration += 1
+            prev_centroids = centroids.copy()
+            iteration += 1
 
 
 if __name__ == "__main__":
